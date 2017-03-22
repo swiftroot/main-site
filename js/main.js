0cf21a7b1432d7a81da3c79f4ac79d8928977e0f
Alek Mlynek <alek@alek.local>
2017-03-21 22:16:25 -0600

    Content changes.

--- js/main.js
+++ js/main.js
@@ -56,11 +56,11 @@
 			});
 
 		// Scrolly.
-			$('.scrolly')
-				.scrolly({
-					speed: 1500,
-					offset: $header.outerHeight()
-				});
+		// 	$('.scrolly')
+		// 		.scrolly({
+		// 			speed: 1500,
+		// 			offset: $header.outerHeight()
+		// 		});
 
 		// Menu.
 			$('#menu')
@@ -78,22 +78,22 @@
 				});
 
 		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
+		// 	if (skel.vars.IEVersion < 9)
+				// $header.removeClass('alt');
 
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
-
-				$window.on('resize', function() { $window.trigger('scroll'); });
-
-				$banner.scrollex({
-					bottom:		$header.outerHeight() + 1,
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); }
-				});
-
-			}
+			// if ($banner.length > 0
+			// &&	$header.hasClass('alt')) {
+      //
+			// 	$window.on('resize', function() { $window.trigger('scroll'); });
+      //
+			// 	$banner.scrollex({
+			// 		bottom:		$header.outerHeight() + 1,
+			// 		terminate:	function() { $header.removeClass('alt'); },
+			// 		enter:		function() { $header.addClass('alt'); },
+			// 		leave:		function() { $header.removeClass('alt'); }
+			// 	});
+      //
+			// }
 
 	});
 
